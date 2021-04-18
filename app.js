a05b7e6f1bdd0cb562efafa048ced8716bf57bfe
umairsolangi391 <umairsolangi821@gmail.com>
2021-04-18 11:05:34 +0500

    done

---
+++ app.js
@@ -0,0 +1,122 @@
+// {chapter no 1}
+//  (Q no 1)
+// alert("Hello World")
+
+// (Q no 2)
+// alert("Error! please enter a valid password.")
+
+// ( Q no 3)
+// alert("welcome to js land... \n Happy coding!")  
+
+// ( Q no 4)
+// alert("welcome to js land...")
+// alert("Happy coding.")
+
+// ( Q no 5)
+// alert("Hello... i can run js through my web browser's consol.")
+
+// ( Q no 6)
+// {/* <script src="app.js"></script> */}
+
+// ( Q no 7)
+// (done script Element )
+
+
+// { chapter no 2}
+// (Q no 1)
+
+// var name ="urooj hameed";
+// alert(name)
+
+// (Q no 2)
+// var myname = ("urooj hameed");
+// alert("urooj")
+
+// // (Q no 3)
+// var helloworld = ("hello world");
+// alert (helloworld) 
+
+// // (Q no 4)
+// var myname =("jhone doe");
+// alert(myname)
+// var age =("15 years old");
+// alert(age)
+// var paragraph =("Certified Mobile Application Development")
+// alert(paragraph)
+
+// // // (Q no 5)
+// var pizza=("pizza \n pizz \n piz \n pi \n p")
+// alert(pizza)
+
+// // // (Q no 6)
+// var email = ("my email address is examlpe.com")
+// alert("my email address is examlpe.com")
+
+// // // (Q no 7)
+// var javascript=("i am trying to learn from the book a smarter way to learn javascript")
+// alert(javascript)
+
+// // // (Q no 8)
+document.write(" <p> Yah! i can write HTML content through javascript </p>" )
+
+// // // (Q no 9)
+// var element=("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// alert(element)
+
+// { chapter no 3}
+// // // (Q no 1)
+// var age=("15")
+// alert("i am  years old "+ age +" age")
+
+// // // (Q no 2)
+// var time =("14")
+// alert("You have visited this site "+time + " time" )
+
+//  // (Q no 3)
+var year =("1999")
+document.write("<p> My birth year is" +   year  +"<br>  data type of my declared variable is number.</p>")
+
+//  // (Q no 4)
+var visitorname =("Jhone Doe")
+var title=("ordered")
+var quantity=("5")
+document.write("Jhone Doe ordered 5 T-shirt(s) on XYZ Clothing store”.")
+
+// { chapter no 4}
+//  // (Q no 1)
+var a=(12); b=(10); c=(20);
+
+//  // (Q no 2)
+// var fav-movie= ;legal
+// var alongvariablename;legal 
+// var X;legal
+// var my_variable;legal
+// var $my_name;legal
+
+// var ++my_variable;illegal
+// var #my_name ;illegal
+// var http:// ;illegal
+// // var first_number;illegal
+// var 3rd name;illegal
+
+//  // (Q no 3)
+document.write("<h3>“Rules for naming JS variables” <br> a) Variable names can only contain , number, string, legal  and  illegal . <br> b) Variables must begin with a letter, number or underscroes.<br>d) Variable names are case sensitive <br> e) Variable names should not be JS a keywords. </h>")
+
+// { chapter no 5}
+//  // (Q no 1)
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+document.write(" <br>The sum of " + num1 + " and " + num2 + " is: " + sum)
+ 
+//  // (Q no 2)
+// var popularNumber = 12 - 24;
+// alert(12 - 24)
+//  var  whatsLeftOver = 10 % 3;
+//  alert(10 % 3)
+//  var anotherNum= 3*6
+//  alert(3*6)
